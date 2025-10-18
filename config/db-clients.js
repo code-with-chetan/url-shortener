8318288ddd70b204ed57ece0508df5a6f559a380
chetan rana <ranachetan455@gmail.com>
2025-10-18 17:20:23 +0545

    url shortener with mongoose

--- config/db-clients.js
+++ config/db-clients.js
@@ -1,4 +1,5 @@
-import { MongoClient } from "mongodb";
-import { env } from "./env.js";
+// import { MongoClient } from "mongodb";
+// import { env } from "./env.js";
 
-export const dbClients = new MongoClient(env.MONGODB_URL); //connect to the database server.
+// export const dbClients = new MongoClient(env.MONGODB_URL);
+//connect to the database server.
